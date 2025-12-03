1331a457fe6c9cf2925f8083550272adffc51e59
Eldos-05 <eldos.220505@gmail.com>
2025-12-03 13:31:23 +0600

    first commit

---
+++ src/main/java/comsep23/srcspro/SrcsProApplication.java
@@ -0,0 +1,13 @@
+package comsep23.srcspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrcsProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrcsProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comsep23/srcspro/SrcsProApplicationTests.java
@@ -0,0 +1,13 @@
+package comsep23.srcspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcsProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
